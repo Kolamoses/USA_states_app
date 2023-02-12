468e1fcbf2ef35e94791a7f32e22f026da2d511f
Kolawole Moses <kolamoses@MacBook-Pro.local>
2023-02-12 16:35:44 -0500

    adding more folder

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "../components/Home";
+
+function HomeScreen() {
+    return (  
+        <React.Fragment>
+            <Home />
+        </React.Fragment>
+    );
+}
+
+export default HomeScreen;
